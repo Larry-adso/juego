7b957be0a0adf255dde90af2b7c84c89760b705c
Larry y angie <windonpc125@gmail.com>
2024-04-13 14:28:43 -0500

    boton

--- views/game/armas_select.php
+++ views/game/armas_select.php
@@ -7,10 +7,13 @@
 </head>
 <body>
     <h1>Armas Aleatorias</h1>
-    <button onclick="buscarArma()">Buscar Arma Aleatoria</button>
+    <button onclick="buscarArma()" id="buscarBtn">Buscar Arma Aleatoria</button>
     <div id="resultado"></div>
     <script>
         function buscarArma() {
+            // Eliminar el botón después de hacer clic en él
+            document.getElementById("buscarBtn").remove();
+
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
@@ -23,4 +26,3 @@
     </script>
 </body>
 </html>
-
