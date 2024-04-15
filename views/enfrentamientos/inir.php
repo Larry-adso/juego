53ba4ed7e6eed5b836b05be2367427f42aea1ca7
larry-adso <windonpc125@gmail.com>
2024-04-14 21:31:19 -0500

    se mejoñññ

---
+++ views/enfrentamientos/inir.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['nickname'])) {
+    echo '<script>
+            alert("Por favor inicie sesión e intente nuevamente");
+            window.location = "../index.php";
+          </script>';
+    session_destroy();
+    die();
+}
+
+
+
+
+?>
