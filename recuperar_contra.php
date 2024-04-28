8cef7faba8634c58877d5906b81de4f627f00133
larry-adso <windonpc125@gmail.com>
2024-04-28 02:55:19 -0500

    se realizo el ajuste correcoto de las interfaces de ganador o perdedor

--- recuperar_contra.php
+++ recuperar_contra.php
@@ -2,61 +2,61 @@
 include("db/PDO.php");
 session_start();
 
-   if (isset($_POST['recuperar']))
-   {
-     $correo=$_POST['correo'];
+if (isset($_POST['recuperar'])) {
+  $correo = $_POST['correo'];
 
-    $sql= $conexion -> prepare ("SELECT * FROM usuarios WHERE correo='$correo'");
-    $sql -> execute();
-    $fila = $sql -> fetchAll(PDO::FETCH_ASSOC);
+  $sql = $conexion->prepare("SELECT * FROM usuarios WHERE correo='$correo'");
+  $sql->execute();
+  $fila = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-   $digitos ="sakur02ue859y2u389rhdewirh102385y1285013289";
-   $longitud= 4;
-   $codigo= substr(str_shuffle($digitos), 0, $longitud);
+  $digitos = "sakur02ue859y2u389rhdewirh102385y1285013289";
+  $longitud = 4;
+  $codigo = substr(str_shuffle($digitos), 0, $longitud);
 
-    $insert= $conexion -> prepare ("UPDATE usuarios SET token='$codigo' Where correo='$correo'");
-    $insert -> execute();
-    $fila1 = $insert -> fetchAll(PDO::FETCH_ASSOC);
+  $insert = $conexion->prepare("UPDATE usuarios SET token='$codigo' Where correo='$correo'");
+  $insert->execute();
+  $fila1 = $insert->fetchAll(PDO::FETCH_ASSOC);
 
-   //codigo de envio
-   $paracorreo = "$correo";
-   $titulo ="Recuperar Contraseña";
-   $msj = "Su codigo de verificacion es: '$codigo'";
-   
-   $tucorreo="From:colaya741@gmail.com";
-   if(mail($paracorreo, $titulo, $msj, $tucorreo))
-   {
-     echo '<script> alert ("Su codigo ha sido enviado al correo anteriormente digitado");</script>';
-     echo '<script>window.location="code.php"</script>';
-   }
-   else{
-       echo "Error";
-   }
+  //codigo de envio
+  $paracorreo = "$correo";
+  $titulo = "Recuperar Contraseña";
+  $msj = "Su codigo de verificacion es: '$codigo'";
 
- }
+  $tucorreo = "From:colaya741@gmail.com";
+  if (mail($paracorreo, $titulo, $msj, $tucorreo)) {
+    echo '<script> alert ("Su codigo ha sido enviado al correo anteriormente digitado");</script>';
+    echo '<script>window.location="code.php"</script>';
+  } else {
+    echo "Error";
+  }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Restablecer Contraseña</title>
   <link rel="stylesheet" href="css/recuperar.css">
 </head>
+
 <body>
   <div class="container">
     <h2>¿Olvidaste tu contraseña?</h2>
-    <form method="post" name="form1" id="form1"  autocomplete="on"> 
+    <form method="post" name="form1" id="form1" autocomplete="on">
       <div class="input-group">
         <label for="email">Correo electrónico:</label>
         <input type="email" id="correo" name="correo" required>
       </div>
       <button type="submit" name="recuperar" class="btn btn-primary">Restablecer</button>
-</form>
+    </form>
   </div>
 
   <script src="script.js"></script>
 </body>
+
 </html>
+
--- views/resultados/win.php
+++ views/resultados/win.php
@@ -1,42 +1,3 @@
-<?php
-session_start();
-
-// Verificar si el usuario está autenticado
-if (!isset($_SESSION['nickname'])) {
-    echo '<script>
-            alert("Por favor inicie sesión e intente nuevamente");
-            window.location = "../index.php";
-          </script>';
-    session_destroy();
-    exit(); // Terminar la ejecución del script después de redireccionar
-}
-
-include("../../db/conexion.php");
-
-// Obtener el nickname del usuario autenticado
-$usuario_autenticado = $_SESSION['nickname'];
-
-// Obtener los datos del usuario autenticado
-$sql_usuario_autenticado = "SELECT u.nickname, a.ruta as avatar
-                            FROM usuarios u
-                            INNER JOIN sala s ON u.nickname = s.nickname
-                            INNER JOIN avatar a ON s.id_avatar = a.id
-                            WHERE u.nickname = ?";
-
-$consulta_usuario_autenticado = $conexion->prepare($sql_usuario_autenticado);
-$consulta_usuario_autenticado->bind_param("s", $usuario_autenticado);
-$consulta_usuario_autenticado->execute();
-$result_usuario_autenticado = $consulta_usuario_autenticado->get_result();
-
-// Verificar si se encontró el usuario autenticado
-if ($result_usuario_autenticado->num_rows > 0) {
-    $row_usuario_autenticado = $result_usuario_autenticado->fetch_assoc();
-    $nombre_usuario_autenticado = $row_usuario_autenticado["nickname"];
-    $avatar_usuario_autenticado = $row_usuario_autenticado["avatar"];
-} else {
-   
-}
-?>
 <!doctype html>
 <html lang="en">
 
@@ -45,20 +6,17 @@ if ($result_usuario_autenticado->num_rows > 0) {
     <!-- Enlaces a Bootstrap y estilos CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../css/victory.css">
+    <script>
+        // Función para redireccionar después de 3 segundos
+        setTimeout(function () {
+            window.location.href = '../lobby.php'; // Redirige a lobby.php
+        }, 3000); // 3000 milisegundos = 3 segundos
+    </script>
 </head>
 
 <body>
     <div class="container">
-        <h1>¡Felicidades! Eres el ganador</h1>
-        <img src="<?php echo $avatar_usuario_autenticado; ?>" alt="Avatar" width="300px">
-        <p>Nombre de usuario: <?php echo $nombre_usuario_autenticado; ?></p>
-        <a href="../lobby.php" class="btn">Volver al lobby</a>
-
     </div>
 </body>
 
 </html>
-<?php
-// Cerrar la conexión con la base de datos
-$conexion->close();
-?>
